a314f3a6ceef144ca923f60ff79230778a57d01a
julianfab <julian.anacona@correounivalle.edu.co>
2020-12-10 23:35:08 -0500

    Agregando excepciones

--- CodePlay/Main.java
+++ CodePlay/Main.java
@@ -1,15 +1,13 @@
 package CodePlay;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.NullPointerException;
-
-
+import java.io.FileNotFoundException;
 
 /**
  * Esta es la clase Principal del programa.
  */
 public class Main {
-	public static void main(String[] args) throws FileNotFoundException, IOException{
+	public static void main(String[] args) throws IOException, FileNotFoundException{
 		EstructuraDatos Datos = new EstructuraDatos();
 		MenuPrincipal Menu = new MenuPrincipal();
 	}
